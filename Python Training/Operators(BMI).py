186f56d64d7f572e08ca04ad4ee52184bc5238f5
Priya Ghadge <88778600+priya6265@users.noreply.github.com>
2021-08-19 19:16:10 +0530

    Create Operators(BMI).py
    Bitwise, Identity and Membership Operators

---
+++ Python Training/Operators(BMI).py
@@ -0,0 +1,24 @@
+#Bitwise Operators
+a=10
+b=20
+print("a&b",a&b)   #AND
+print("a|b",a|b)   #OR
+print("~a",~a)     #NOT
+print("a^b",a^b)   #XOR
+print("a<<b",a<<b) #Left shift
+print("a>>b",a>>b) #Right shift
+
+#Membership operators
+list=["A","B","C","D","E"]
+print("A" in list)
+print("A" not in list)
+print("F" in list)
+print("F" not in list)
+str="This is Python programming"
+print("is" in str)
+
+#Identity operators
+a=True
+b=False
+print(a is b)
+print(a is not b)

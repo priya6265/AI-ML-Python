47563e287c542508c56d17902d94743913a47ffb
Priya Ghadge <88778600+priya6265@users.noreply.github.com>
2021-08-21 14:39:06 +0530

    Create Is_vowel_consonant.py

---
+++ Python Training/Is_vowel_consonant.py
@@ -0,0 +1,8 @@
+#to check vowel or consonant
+c= input(" Enter Your Character : ")
+
+if(c == 'a' or c== 'e' or c== 'i' or c== 'o' or c == 'u' or c == 'A'
+       or c == 'E' or c == 'I' or c == 'O' or c== 'U'):
+    print("The entered Character ", c, "is a Vowel")
+else:
+    print("The entered Character ", c, "is a Consonant")
